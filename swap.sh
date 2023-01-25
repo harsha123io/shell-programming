cfb36a6961b1c0102bea6ea9bec07a867ab4d247
harsha123io <123438408+harsha123io@users.noreply.github.com>
2023-01-24 20:44:50 -0800

    Add files via upload

---
+++ swap.sh
@@ -0,0 +1,13 @@
+echo "enter the first number"
+read a
+echo "entet the second number"
+read b
+echo "before swappinng"
+echo "first number: $a"
+echo "second number: $b"
+c=$a
+a=$b
+b=$c
+echo "after swapping"
+echo "first nnumber: $a"
+echo "second number: $b"
